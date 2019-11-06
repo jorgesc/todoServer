82a1a38f91d5ce87a8736ce5945c29055a1b2ea9
Jorge Santamaría <jsc_@gmx.es>
2019-11-06 18:48:20 +0100

    Only title and description are taken from request body now, the rest of the fields are generated on the controller

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,13 +1,24 @@
 import {Request, Response} from "express";
 import TaskModel from "../models/TaskModel";
 
+const NOT_LOGGED_IN = (res: Response) =>
+  res.status(401).json({status: "error", result: "User not logged in"});
+
 export default {
   createNewTask: async (req: Request, res: Response): Promise<Response> => {
-    const task = new TaskModel(req.body);
-    task.createdBy = req.session.userId;
-    task.createdOn = new Date();
-    task.completed = false;
-    task.children = [];
+    if (!req.session || !req.session.userId) return NOT_LOGGED_IN(res);
+
+    const {title, description} = req.body;
+    const taskData = {
+      title,
+      description,
+      createdBy: req.session.userId,
+      createdOn: new Date(),
+      completed: false,
+      children: [],
+    };
+
+    const task = new TaskModel(taskData);
     return res.status(201).json({status: "ok", result: await task.save()});
   },
 };
--- src/routes/__tests__/TasksRoutes.test.ts
+++ src/routes/__tests__/TasksRoutes.test.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import request from "supertest-session";
 import App from "../../App";
 import {ITask} from "../../types/types";
@@ -39,6 +40,20 @@ describe("Task Routes: Create task", () => {
     userId = user.body.result._id;
   });
 
+  it("Can't create anything when user is not logged in", async () => {
+    const taskData = {
+      title: "Test task",
+      description: "Whatever",
+    };
+
+    const taskResponse = await request(App)
+      .post("/tasks")
+      .send(taskData);
+    expect(taskResponse.statusCode).toEqual(401);
+    expect(taskResponse.body.status).toEqual("error");
+    expect(taskResponse.body.result).toEqual("User not logged in");
+  });
+
   it("Create a simple task with no parent", async () => {
     const taskData = {
       title: "Test task",
@@ -46,6 +61,7 @@ describe("Task Routes: Create task", () => {
     };
 
     const taskResponse = await testSession.post("/tasks").send(taskData);
+    expect(taskResponse.statusCode).toEqual(201);
     expect(taskResponse.body.status).toEqual("ok");
     expect(taskResponse.body.result).toMatchObject(taskData);
     expect(taskResponse.body.result.createdBy).toEqual(userId);
@@ -70,5 +86,31 @@ describe("Task Routes: Create task", () => {
 
     expect(dbTask._id.toString()).toBe(returnedTask._id);
     expect(Array.from(dbTask.children)).toEqual(returnedTask.children);
+  });
+
+  it("Overrides _id, createdBy, etc post fields if the request includes it", async () => {
+    const taskData = {
+      title: "Test task",
+      description: "Whatever",
+      createdBy: mongoose.Types.ObjectId(),
+      _id: mongoose.Types.ObjectId(),
+      createdOn: new Date(2011, 4, 6, 0, 0, 0),
+      completed: true,
+    };
+
+    const taskResponse = await testSession.post("/tasks").send(taskData);
+    expect(taskResponse.statusCode).toEqual(201);
+    expect(taskResponse.body.status).toEqual("ok");
+
+    const tasksOnDb = await TaskModel.find({}).exec();
+    expect(tasksOnDb).toHaveLength(1);
+
+    const dbTask = tasksOnDb[0];
+    expect(dbTask.title).toEqual(taskData.title);
+    expect(dbTask.description).toEqual(taskData.description);
+    expect(dbTask.createdBy).not.toEqual(taskData.createdBy);
+    expect(dbTask.createdOn).not.toEqual(taskData.createdOn);
+    expect(dbTask._id).not.toEqual(taskData._id);
+    expect(dbTask.completed).not.toEqual(taskData.completed);
   });
 });
